7c0ce2648af774109d49f6c0f5caf038b8dbfca5
khushirajput03 <khushikhushi89978@gmail.com>
2024-12-17 09:54:59 +0000

    convertion and opertation of datatype

---
+++ 01_basics/converstionoperation.js
@@ -0,0 +1,38 @@
+let score = "33";
+
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//33 => 33
+// 33abc => NaN but type is number
+//true = 1  false = 0
+
+//let isLoggedIn =  1;
+
+//let isBooleanNumber = Boolean(isLoggedIn)
+//console.log(isBooleanNumber)
+
+/*let isLoggedIn =  "";
+
+let isBooleanNumber = Boolean(isLoggedIn)
+console.log(isBooleanNumber)*/
+
+let isLoggedIn =  "khushi";
+
+let isBooleanNumber = Boolean(isLoggedIn)
+//console.log(isBooleanNumber)
+
+// 1 => true  // 0 => false
+// "" => false
+// "khushi" = true
+
+
+let stringNumber = 66
+
+let isStringNumber = String(stringNumber);
+console.log(isStringNumber)
+console.log(typeof isStringNumber)
