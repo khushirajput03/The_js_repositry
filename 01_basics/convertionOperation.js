c623979717f221252a1b86b4cfd6141417cdd7b1
khushirajput03 <khushikhushi89978@gmail.com>
2024-12-17 13:20:17 +0000

    link to study operators

---
+++ 01_basics/convertionOperation.js
@@ -0,0 +1,81 @@
+let score = "33";
+
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+//33 => 33
+// 33abc => NaN but type is number
+//true = 1  false = 0
+
+//let isLoggedIn =  1;
+
+//let isBooleanNumber = Boolean(isLoggedIn)
+//console.log(isBooleanNumber)
+
+/*let isLoggedIn =  "";
+
+let isBooleanNumber = Boolean(isLoggedIn)
+console.log(isBooleanNumber)*/
+
+let isLoggedIn =  "khushi";
+
+let isBooleanNumber = Boolean(isLoggedIn)
+//console.log(isBooleanNumber)
+
+// 1 => true  // 0 => false
+// "" => false
+// "khushi" = true
+
+
+let stringNumber = 66
+
+let isStringNumber = String(stringNumber);
+//console.log(isStringNumber)
+//console.log(typeof isStringNumber)
+
+//**************************************OPERATORS**************************************************** 
+
+let numValue = 6;
+let negNum = -numValue
+//console.log(negNum);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+
+let str1 = "hello"
+let str2 = " khushi"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let num1 , num2 , num3
+
+num1 , num2 , num3 = 2+2
+
+let GameCounter = 100
+//GameCounter++;
+++GameCounter;
+console.log(GameCounter)
+
+//Link to study 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_operators
